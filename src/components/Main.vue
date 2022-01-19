fc80ee3e0b1f0c8617858d48c62dff019e630da1
otuoio <mariocoppo91@gmail.com>
2022-01-19 15:38:41 +0100

    add ul with movies info

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>ciao</div>
+  <div class="container">
+      <ul v-for="(movie, index) in movies"
+          :key="index">
+          <li>
+              {{ movie.title}}
+          </li>
+          <li>
+              {{ movie.original_title}}
+          </li>
+          <li>
+              {{ movie.original_language}}
+          </li>
+          <li>
+              {{ movie.vote_count}}
+          </li>
+      </ul>
+  </div>
 </template>
 
 <script>
@@ -9,17 +25,20 @@ export default {
     name: "Main",
     data() {
         return {
+            movies: [],
             queryApi: 'https://api.themoviedb.org/3/search/movie',
             apiKey: '?api_key=' + '981731b128a2c3353bf07ea0418b25f5', //mia api_key
+            lang: '&language=',
+            query: '&query='
         }
     },
     created() {
 
-        // console.log('https://api.themoviedb.org/3/search/movie?api_key=981731b128a2c3353bf07ea0418b25f5?query=batman');
         axios
-        .get('https://api.themoviedb.org/3/search/movie?api_key=981731b128a2c3353bf07ea0418b25f5?query=batman')
+        .get(this.queryApi+this.apiKey+this.lang+'it-IT'+this.query+'batman')
         .then(result => {
-            console.log(result);
+            console.log(result.data.results);
+            this.movies = result.data.results;
         })
         .catch(error => {
             console.log(error);
