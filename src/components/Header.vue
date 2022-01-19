6f1fc3f524d1ca4567777f2d550fd57c9ab7ba0c
otuoio <mariocoppo91@gmail.com>
2022-01-19 15:24:48 +0100

    add axios get

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <input type="text" name="text" id="text" placeholder="Search">
+      <button class="btn">Cerca</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,11 +7,23 @@ import axios from 'axios';
 
 export default {
     name: "Main",
+    data() {
+        return {
+            queryApi: 'https://api.themoviedb.org/3/search/movie',
+            apiKey: '?api_key=' + '981731b128a2c3353bf07ea0418b25f5', //mia api_key
+        }
+    },
     created() {
+
+        // console.log('https://api.themoviedb.org/3/search/movie?api_key=981731b128a2c3353bf07ea0418b25f5?query=batman');
         axios
-        .get()
-        .then()
-        .catch();
+        .get('https://api.themoviedb.org/3/search/movie?api_key=981731b128a2c3353bf07ea0418b25f5?query=batman')
+        .then(result => {
+            console.log(result);
+        })
+        .catch(error => {
+            console.log(error);
+        });
     }
 }
 </script>
