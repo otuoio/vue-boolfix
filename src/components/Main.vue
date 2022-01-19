65925b846371f9c34f8d39ffd95b80e4c7ef9dad
otuoio <mariocoppo91@gmail.com>
2022-01-19 11:24:27 +0100

    add init config

---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>ciao</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "Main",
+    created() {
+        axios
+        .get()
+        .then()
+        .catch();
+    }
+}
+</script>
+
+<style>
+
+</style>
