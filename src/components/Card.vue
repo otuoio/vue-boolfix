b032ca90bea9c24cc6a3018b1e728421afbcc72c
otuoio <mariocoppo91@gmail.com>
2022-01-21 21:14:33 +0100

    add milestone 4

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="card">
-      <div @mouseover="showInfo = true" @mouseleave="showInfo=false" class="poster">
+      <div class="poster">
           <img v-if="isNull(poster)" :src="poster" :alt="alt">
           <div v-else class="not-poster">
               {{ originalTitle}}
           </div>
       </div>
-      <div v-show="showInfo" class="info">
-          <h2 class="title">Title: {{ title }}</h2>
-        <h3 class="org_title">Original Title: {{ originalTitle }}</h3>
+      <div class="info">
+        <h2 class="title"><b>Titolo: </b> {{ title }}</h2>
+        <h3 class="org_title"><b>Titolo originale: </b> {{ originalTitle }}</h3>
         <div class="flag">
-            <country-flag v-if="isAvailable()" :country='getFlags(language)' size='small' shadow />
+            <country-flag v-if="isAvailable()" :country='getFlags(language)' size='small' shadow class="lang"/>
             <span v-else>{{ language }}</span>
         </div>
         <div class="vote">
           <font-awesome-icon v-for="(item, index) in votes"
-            :key="index" :icon="[votesArray(index, vote), 'star']" /> 
+            :key="index" :icon="[votesArray(index, vote), 'star']" class="star" /> 
         </div>
         <p><b>Overview: </b>{{ overview }}</p>
         </div>
@@ -98,6 +98,9 @@ export default {
             } else {
                 return 'far';
             }
+        },
+        isHover() {
+            
         }
     }
 }
@@ -107,8 +110,10 @@ export default {
 @import '../assets/scss/partials/_mixins.scss';
 
 .card {
+    margin: 1em;
     flex-basis: 22%;
     text-align: center;
+    position: relative;
     .poster {
         height: 70vh;
         border: 1px solid white;
@@ -116,6 +121,39 @@ export default {
             width: 100%;
             height: 100%;
         }
+    }
+    &:hover .poster {
+        display: none;
+    }
+    .info {
+        display: none;
+        @include absolute(0, 0);
+        background-color: black;
+        width: 100%;
+        height: 70vh;
+        color: white;
+        text-align: start;
+        padding: .5em;
+        overflow: auto;
+        h2,
+        h3,
+        p,
+        .flag span,
+        .star {
+            margin: .5em;
+        }
+        h2,
+        h3,
+        p {
+            font-size: 1em;
+            font-weight: 300;
+        }
+        .star {
+            color: rgb(255, 208, 0);
+        }
+    }
+    &:hover .info {
+        display: block;
     }
 }
 </style>
