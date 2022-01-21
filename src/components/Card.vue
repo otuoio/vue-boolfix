66b3f7bbeebbf751678c9344fdde001c3d5d2b4b
otuoio <mariocoppo91@gmail.com>
2022-01-21 11:26:26 +0100

    add stars regular and solid icons

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,14 +3,27 @@
         {{ title }}
         {{ originalTitle }}
         <country-flag :country='getFlags(language)' size='small' shadow />
+        <i class="far fa-star"></i>
+        <i class="fas fa-star"></i>
+        <!-- <font-awesome-icon icon="user-secret" /> -->
+        <!-- <font-awesome-icon icon="'star'" /> -->
+        <font-awesome-icon :icon="['far', 'star']" />
+        <font-awesome-icon :icon="['fas', 'star']" />
         {{ vote }}
         <img :src="poster" :alt="alt">
   </div>
 </template>
 
 <script>
+
+
+
+
 export default {
     name: 'Card',
+    compontent: {
+
+    },
     props: {
         title: {
             type: String,
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import CountryFlag from 'vue-country-flag';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(fasStar, farStar);
 
 Vue.component('country-flag', CountryFlag);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
